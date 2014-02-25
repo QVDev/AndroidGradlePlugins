0d0d0d30c4c052ac1f85301cb4b9220f4b48cd7b
QVDev <feedbackandroid@gmail.com>
2014-02-25 16:16:39 +0100

    Implemented checkstyle coverage

---
+++ javascripts/checkstyle.js
@@ -0,0 +1,16 @@
+console.log('This would be the main JS file.');
+
+xhttp=new XMLHttpRequest();
+xhttp.open("GET","/reports/checkstyle.xml",false);
+xhttp.send();
+xmlDoc=xhttp.responseXML;
+
+totalerrors = xmlDoc.getElementsByTagName( "error" ).length;
+document.write(totalerrors, ' Errors<br><br>');
+
+// for (var i=0;i<totalerrors;i++)
+// { 
+// 	txt=xmlDoc.getElementsByTagName("error")[i].getAttribute("message");
+// 	document.write(txt, '<br>');
+// }
+
