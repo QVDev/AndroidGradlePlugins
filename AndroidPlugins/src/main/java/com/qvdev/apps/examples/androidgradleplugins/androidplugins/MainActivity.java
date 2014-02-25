5e8c3fdba25f17680c2e58730e9fa3e779ca0b3d
QVDev <feedbackandroid@gmail.com>
2014-02-25 12:49:01 +0100

    Added wrong method name to test Checkstyle

--- AndroidPlugins/src/main/java/com/qvdev/apps/examples/androidgradleplugins/androidplugins/MainActivity.java
+++ AndroidPlugins/src/main/java/com/qvdev/apps/examples/androidgradleplugins/androidplugins/MainActivity.java
@@ -16,10 +16,13 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private void GetItems() {
     }
 
     @Override
